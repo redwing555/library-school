bf0c539843efafa09d2464be0700d51f327d2fc4
Hichami Houssam <hichami.houssam@gmail.com>
2021-12-13 18:47:47 +0100

    randomizing id

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ class Person
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = 111
+    @id =  Random.rand(1...1_000_000)
     @name = name
     @parent_permission = parent_permission
     @age = age
