86655de562ad938669b3dcc6c168eab71dca69d4
Hichami Houssam <hichami.houssam@gmail.com>
2021-12-14 15:53:48 +0100

    creating a bug

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source 'https://rubygems.org'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 gem 'rubocop', '>= 1.0', '< 2.0'
+gem 'pry', '~> 0.13.1'
 
 # gem "rails"
--- person.rb
+++ person.rb
@@ -27,6 +27,22 @@ class Person
   private :of_age?
 end
 
-# person = Person.new(19, 'houssammmmmmam')
-# person.validate_name
-# p person.name
+person1 = Person.new(20, 'mark')
+person2 = Person.new(18, 'john')
+person3 = Person.new(14, 'alice')
+person4 = Person.new(20, 'bob')
+person5 = Person.new(15, 'mike')
+person6 = Person.new(10, 'darius')
+
+arr = [person1, person2, person3, person4,person5,person6]
+new_arr = []
+
+# array of people that have only odd age that is less than or equal to 14 or is a multiples of 3
+
+arr.each do |person|
+  new_arr << person.name if person.age.odd? && person.age >= 14 || person.age%3 == 0
+end
+
+p new_arr          #   ["mike"]
+
+
