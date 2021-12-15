2869ad24a29a60327654fcad40c3c411de85872e
Hichami Houssam <hichami.houssam@gmail.com>
2021-12-15 16:08:18 +0100

    correcting rentals

--- rental.rb
+++ rental.rb
@@ -1,13 +1,14 @@
+require 'date'
 class Rental
     attr_accessor :date
     attr_reader :person, :book
 
-    def initialize(date:, person:, book:)
+    def initialize(date: Datetime.now(), person:, book:)
         @date = date
         @person = person
         @book = book
-    end
 
-    person.rentals.push(self)
-    book.rentals.push(self)
+        person.rentals.push(self)
+        book.rentals.push(self)
+    end
 end
