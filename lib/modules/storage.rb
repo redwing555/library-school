f48464911d7fa2d9c6928acf463b94cba83d7171
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-12-22 17:03:02 +0200

    Fix linters

--- lib/modules/storage.rb
+++ lib/modules/storage.rb
@@ -14,8 +14,6 @@ module Storage
     from_file_to_rentals(rentals) if rentals
   end
 
-
-
   def from_books_to_file
     books_json = []
     @books.each do |book|
@@ -27,7 +25,6 @@ module Storage
                            })
 
     File.write('books.json', books_json)
-
   end
 
   def from_people_to_file
@@ -46,29 +43,29 @@ module Storage
   def from_rentals_to_file
     rentals_json = []
     @rentals.each do |rental|
-    rentals_json.push(rental.to_json)
+      rentals_json.push(rental.to_json)
     end
 
     rentals_json = JSON.dump({
                                rentals: rentals_json
                              })
-    File.write('rentals.json',rentals_json)
+    File.write('rentals.json', rentals_json)
   end
 
   def from_file_to_books(books)
-    book_data = JSON.parse(books)["books"]
+    book_data = JSON.parse(books)['books']
 
     return if book_data == []
-    
+
     book_data.each do |object|
       file = JSON.parse(object)
-      created_book = Book.new(title: file['title'],author: file['author'])
+      created_book = Book.new(title: file['title'], author: file['author'])
       @books.push(created_book) if file
     end
   end
 
   def from_file_to_people(people)
-    people_data = JSON.parse(people)["people"]
+    people_data = JSON.parse(people)['people']
     return if people_data == []
 
     people_data.each do |object|
@@ -78,26 +75,25 @@ module Storage
       teacher = Teacher.new(age: file['age'], name: file['name'], specialization: file['specialization'])
 
       if file['classname'] == 'Student' && file
-        @people.push(student) 
+        @people.push(student)
       else
-        @people.push(teacher)  
+        @people.push(teacher)
       end
     end
   end
 
   def from_file_to_rentals(rentals)
-    rentals_data = JSON.parse(rentals)["rentals"]
+    rentals_data = JSON.parse(rentals)['rentals']
     return if rentals_data == []
 
     rentals_data.each do |object|
-      json_object = JSON.parse(object) 
-      rental = Rental.new(date:json_object['date'],person: @people[person_index_lookup(json_object)],book: @books[book_index_lookup(json_object)])
-     
+      json_object = JSON.parse(object)
+      rental = Rental.new(date: json_object['date'], person: @people[person_index_lookup(json_object)],
+                          book: @books[book_index_lookup(json_object)])
+
       @rentals.push(rental) if json_object
     end
   end
-
-
 
   def person_index_lookup(object)
     object = JSON.parse(object['person'])
@@ -108,17 +104,17 @@ module Storage
     parent_permission = object['parent_permission']
 
     if classname == 'Teacher'
-        @people.each_with_index do |person, index|
-             if ( classname == person.class.to_s && name == person.name && age == person.age &&  specialization == person.specialization)
-                return index 
-             end
+      @people.each_with_index do |person, index|
+        if classname == person.class.to_s && name == person.name && age == person.age && specialization == person.specialization
+          return index
         end
+      end
     else
-        @people.each_with_index do |person, index|
-             if (age == person.age and name == person.name and parent_permission == person.parent_permission and classname == person.class.to_s)
-                return index
-             end
+      @people.each_with_index do |person, index|
+        if age == person.age and name == person.name and parent_permission == person.parent_permission and classname == person.class.to_s
+          return index
         end
+      end
     end
   end
 
