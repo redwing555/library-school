55dcfc6dfb00c25c671ca133fad77b3c785f70da
Hichami Houssam <hichami.houssam@gmail.com>
2021-12-22 15:18:56 +0100

    creating storage module

---
+++ lib/modules/storage.rb
@@ -0,0 +1,13 @@
+require_relative '../classes/person'
+require_relative '../classes/book'
+require_relative '../classes/classroom'
+require_relative '../classes/student'
+require_relative '../classes/rental'
+require_relative '../classes/teacher'
+require 'json'
+
+module Storage
+
+end
+
+
