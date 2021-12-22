cd0754fe0d68257860335ccca2a01950bb862685
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-12-22 16:32:22 +0200

    Send user input on books to file

--- lib/modules/storage.rb
+++ lib/modules/storage.rb
@@ -7,7 +7,25 @@ require_relative '../classes/teacher'
 require 'json'
 
 module Storage
+    def from_file(people: nil, books: nil, rentals: nil)
+        from_file_to_books(books) if books
+    end
 
+    def from_books_to_file
+        books_json = []
+        @books.each do |book|
+          books_json.push(book.to_json)
+        end
+    
+        books_json = JSON.dump({
+                                 books: books_json
+                               })
+    
+        File.write('books.json', books_json)
+    
+    end
+
+    
 end
 
 
