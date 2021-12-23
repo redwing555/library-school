67bec167935131f98adce52c973813559d16d92f
Hichami Houssam <hichami.houssam@gmail.com>
2021-12-23 11:41:51 +0100

    fixing robocop offenses

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,22 +1,20 @@
 require_relative '../lib/classes/classroom'
 
 describe Classroom do
-
-    context 'testing a classroom class' do
-        it 'should create new classroom instance correctly' do
-            classroom = Classroom.new('Philosophy')
-            expect(classroom.label).to eq 'Philosophy'
-        end
+  context 'testing a classroom class' do
+    it 'should create new classroom instance correctly' do
+      classroom = Classroom.new('Philosophy')
+      expect(classroom.label).to eq 'Philosophy'
     end
+  end
 
-    context 'testing a classroom class' do
-        it 'should add student to classroom' do
-            classroom = Classroom.new('Philosophy')
-            
-            expect(classroom.students).to eq []
-            classroom.add_student(Student.new(age:18, name:'bob', parent_permission:true))
-            expect(classroom.students[0].name).to eq 'bob'
-            
-        end
+  context 'testing a classroom class' do
+    it 'should add student to classroom' do
+      classroom = Classroom.new('Philosophy')
+
+      expect(classroom.students).to eq []
+      classroom.add_student(Student.new(age: 18, name: 'bob', parent_permission: true))
+      expect(classroom.students[0].name).to eq 'bob'
     end
+  end
 end
--- spec/rentals_spec.rb
+++ spec/rentals_spec.rb
@@ -3,32 +3,29 @@ require_relative '../lib/classes/book'
 require_relative '../lib/classes/rental'
 require 'json'
 
-
 describe Rental do
-    context 'Instance of rental class' do
-        it 'Should instantiate the rental class correctly' do
-            book1 = Book.new(title:'The lord of the rings' , author:'Tolkien')
-            person1 = Person.new(age: 15, name:'Bob', parent_permission: true)
-    
-            rental1 = Rental.new(date:'2020/20/20',person:person1,book:book1)
-    
-            expect(person1.rentals[0]).to eq rental1
-            expect(book1.rentals[0]).to eq rental1
-        end
-    
-        it 'Json state values should equal class instance values' do
-            book1 = Book.new(title:'The lord of the rings' , author:'Tolkien')
-            student1 = Student.new(age: 15, name:'Bob', parent_permission: true)
-            rental1 = Rental.new(date:'2020/20/20',person:student1, book:book1)
-            
-            book_json = book1.to_json
-            student_json = student1.to_json
-    
-    
-            expect(JSON.parse(rental1.to_json)["date"]).to eq rental1.date
-            expect(JSON.parse(rental1.to_json)["person"]).to eq student_json
-            expect(JSON.parse(rental1.to_json)["book"]).to eq book_json
-        end
-    end    
-end
+  context 'Instance of rental class' do
+    it 'Should instantiate the rental class correctly' do
+      book1 = Book.new(title: 'The lord of the rings', author: 'Tolkien')
+      person1 = Person.new(age: 15, name: 'Bob', parent_permission: true)
 
+      rental1 = Rental.new(date: '2020/20/20', person: person1, book: book1)
+
+      expect(person1.rentals[0]).to eq rental1
+      expect(book1.rentals[0]).to eq rental1
+    end
+
+    it 'Json state values should equal class instance values' do
+      book1 = Book.new(title: 'The lord of the rings', author: 'Tolkien')
+      student1 = Student.new(age: 15, name: 'Bob', parent_permission: true)
+      rental1 = Rental.new(date: '2020/20/20', person: student1, book: book1)
+
+      book_json = book1.to_json
+      student_json = student1.to_json
+
+      expect(JSON.parse(rental1.to_json)['date']).to eq rental1.date
+      expect(JSON.parse(rental1.to_json)['person']).to eq student_json
+      expect(JSON.parse(rental1.to_json)['book']).to eq book_json
+    end
+  end
+end
